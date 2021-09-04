8b362f30ad992d169b37be0c0bc6992ec2a09d60
zeta <812217440@qq.com>
2021-09-04 10:06:00 +0800

    9/4

---
+++ Java_Data_Structure/src/com/atguigu/search/InsertSearch.java
@@ -0,0 +1,17 @@
+package com.atguigu.search;
+
+/**
+ * @author Zeta
+ * @version 1.0
+ * @date 2021/9/4 10:04
+ */
+public class InsertSearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+
+    }
+    public static boolean insertSearch(int[] arr, int target){
+
+    }
+
+}
