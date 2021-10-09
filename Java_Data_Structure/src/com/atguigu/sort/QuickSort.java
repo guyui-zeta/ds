58dee3d85950f0e32e1ca433c7178a80b3c7c150
zeta <812217440@qq.com>
2021-10-09 22:17:50 +0800

    10-9：快排bug： 1.要先排右侧，再拍左侧 2.每次排时要确保low<high sb bilibili2

--- Java_Data_Structure/src/com/atguigu/sort/QuickSort.java
+++ Java_Data_Structure/src/com/atguigu/sort/QuickSort.java
@@ -4,44 +4,40 @@ import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
-        int[] array = {-9,78,0,23,-567,70, -1,900, 4561};
+        int[] array = {10,10,0,23,-567,70, -1,900, 4561};
         quickSort(array,0,array.length-1);
         System.out.println(Arrays.toString(array));
     }
 
-    public static void quickSort(int[] array, int start,int end)
-    {
+    public static void quickSort(int[] array, int start,int end) {
         //终止条件
-        if(start >= end)
-        {
+        if(start > end) {
             return;
         }
         int low = start;
         int high = end;
-        int pivot = array[(start + end)/2];
-        int temp = 0;
-        while(low < high)
-        {
-            //pivot左边的值比pivot小，直到找到大于等于pivot的
-            while(array[low] < pivot)
-            {
-                low+=1;
-            }
-            //pivot右边的值比pivot大，直到找到小于等于pivot的值
-            while(array[high] > pivot)
-            {
+        //设置左边第一位为标准位
+        int compare = array[start];
+        while(low < high) {
+            //【重要：先从左边开始找！！！！！！！！！！！！！！！】
+            //找到右边异类：比compare小的【重要：每次比较时，也要比较low和high的位置关系】
+            while(array[high] >= compare && low < high) {
                 high-=1;
             }
-            //经过两个循环，找到了pivot两边的异类值，并对他们进行交换
-            temp = array[low];
+            //找到左边异类：比compare大的
+            while(array[low] <= compare && low < high) {
+                low+=1;
+            }
+            //经过两个循环，找到了两边的异类值，并对他们进行交换
+            int temp = array[low];
             array[low] = array[high];
             array[high] = temp;
         }
-        //结束while循环时表明；low=high=pivot，此时该排序已完成，左右大小以大局确定
+        //交换完异类后，交换标准位和low的值
+        array[start] = array[low];
+        array[low] = compare;
         //下一个递归的范围为：low-1和low+1
         quickSort(array,start,low-1);
         quickSort(array,low+1,end);
-
-
     }
 }
