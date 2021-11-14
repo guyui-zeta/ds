09695ab33aa1c3154105ea786ac42df00f3b923f
zeta <812217440@qq.com>
2021-11-14 16:06:29 +0800

    11-14 feature分支新增功能

--- Java_Data_Structure/src/com/atguigu/test.java
+++ Java_Data_Structure/src/com/atguigu/test.java
@@ -1,35 +1,17 @@
 package com.atguigu;
 
+
+/**
+ * feature分支新增功能
+ */
 public class test {
     public static void main(String[] args) {
-        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
-        int[][] res = setZeroes(matrix);
-        for(int i = 0; i < res.length; i++){
-            for(int j = 0; j < res[i].length; j++){
-                System.out.println(res[i][j]);
-            }
-        }
+        Integer a = new Integer(100);
+        Integer b = new Integer(100);
+        System.out.println(a == b);
+        System.out.println(a.equals(b));
     }
-    public static int[][] setZeroes(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = col[j] = true;
-                }
-            }
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i] || col[j]) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        return matrix;
-    }
+
 
 
 }
