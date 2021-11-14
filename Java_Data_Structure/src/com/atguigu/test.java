980629b7be8b4fd202d7fa36e0743f40f945c72e
zeta <812217440@qq.com>
2021-11-14 16:08:50 +0800

    11-14 feature分支优化功能

--- Java_Data_Structure/src/com/atguigu/test.java
+++ Java_Data_Structure/src/com/atguigu/test.java
@@ -8,9 +8,10 @@ public class test {
     public static void main(String[] args) {
         Integer a = new Integer(100);
         Integer b = new Integer(100);
-        System.out.println(a == b);
         System.out.println(a.equals(b));
+        System.out.println("测试feature修改功能");
     }
+
 
 
 
